b508c8481766f188aa238cfcfeca5c0db9492bf6
Akshaya <akshayarh@gmail.com>
2021-04-04 14:56:59 +0530

    first commit

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent {
+  title = 'StringInterpolation';
+
+  fname='Akshaya';
+  lname='Hingangave';
+  email='akshayarh@gmail.com';
+  number='8597462951';
+  password1='**********';
+  password2='**********';
+  city1='Pune';
+  city2='Sangli';
+
+
+}
